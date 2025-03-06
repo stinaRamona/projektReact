ea50ae922e91c474acc3454f19cbacbdcc23d863
Stina Persson <stpe1901@student.miun.se>
2025-03-06 09:16:52 +0100

    fått ut volyminfo och id

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,26 +2,23 @@ import { useState } from "react";
 
 const SearchBar = () => {
 
-    interface BookListInterface {
-        //SVAR FRÅN API ()DET BLIR SÅ LåNGT? 
-    }
-
     //states 
-    const [searchData, setSearchData] = useState<string>();
-    const [bookList, setBookList] = useState<BookListInterface | null>(); 
+    const [searchData, setSearchData] = useState<string>(); 
 
     const getSearchedBooks = async (event: any) => {
         //ska hämta böcker baserat på sökresultat
         event.preventDefault(); 
 
         try {
+
             const response = await fetch("https://www.googleapis.com/books/v1/volumes?q=" + searchData)
 
             const data = await response.json(); 
 
-            setBookList(data); 
+            let booklist = data; 
 
-            console.log(bookList); 
+            console.log(booklist.items[0].volumeInfo); //Får ut information om volymen 
+            console.log("Bok-ID:" + booklist.items[0].id); //Får ut bokid på boken 
 
         } catch(error) {
 
@@ -34,6 +31,7 @@ const SearchBar = () => {
     <>
       <form onSubmit={getSearchedBooks}>
         <label htmlFor="searchValue">Sök efter titel, författare eller tema:</label>
+        <br />
         <input type="text" name="searchValue" id="searchValue" value={searchData}
         onChange={(event) => setSearchData(event.target.value)} //värdet från baren.  
         />
