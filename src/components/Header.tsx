bcb5d93db2503455022d7b659e8e737934c798d1
Stina Persson <stpe1901@student.miun.se>
2025-03-06 11:55:35 +0100

    Stylat header lite, lagt till bakgrudsbild för header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,19 @@
 import { NavLink } from "react-router-dom"; 
+import Wave from "../assets/wave.svg"; 
+import "../css/Header.css"; 
 
 const Header = () => {
   return (
+    <>
     <header>
+        <h1 id="logo">Logga</h1>
         <ul>
-            <li><NavLink to="/">Start</NavLink></li>
-            <li><NavLink to="/login">Logga in</NavLink></li>
+            <li><NavLink className="nav-link" to="/">Start</NavLink></li>
+            <li><NavLink className="nav-link" to="/login">Logga in</NavLink></li>
         </ul>
     </header>
+    <img src={Wave} alt="en våg som bakgrund för navigationen" />
+    </>
   )
 }
 
