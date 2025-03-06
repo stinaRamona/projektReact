38db4b9c7d6c326c65ad44684baab2ee9a04d67c
Stina Persson <stpe1901@student.miun.se>
2025-03-06 15:56:53 +0100

    Fungerande registrering implementerad

--- src/components/InfoDiv.tsx
+++ src/components/InfoDiv.tsx
@@ -3,7 +3,7 @@ import "../css/InfoDiv.css";
 const InfoDiv = () => {
   return (
     <div id="infoDiv">
-        <h3>Boksidan hjälper dig att hitta i bokdjungeln</h3>
+        <h3>Boksidan som hjälper dig att hitta i bokdjungeln</h3>
         <p>
         Hitta böcker, läs användarrecensioner och ta beslut om vilken bok att läsa härnäst. 
         Skapa ett konto och skriv egna recensioner för att hjälpa andra hitta sin nästa favoritbok.
--- src/components/RegisterUserForm.tsx
+++ src/components/RegisterUserForm.tsx
@@ -1,21 +1,65 @@
 import InfoDiv from "./InfoDiv"
 import "../css/UserForm.css"; 
+import { useState } from "react";
 
 const RegisterUserForm = () => {
+    //interface för ny användare
+    interface NewUser {
+        user_name: string, 
+        email: string, 
+        password: string
+    }
+
+    //state för användare och för errormeddelanden 
+    const [userData, setUserData] = useState<NewUser>({user_name:"", email:"", password:""}); 
+    const [error, setError] = useState<string>(); 
+
+    const registerUser = async (event : any) => {
+
+        event.preventDefault(); 
+
+        try {
+            const response = await fetch("http://localhost:3000/adduser", {
+                method: "POST", 
+                headers: {
+                    "Content-Type": "application/json"
+                }, 
+                body: JSON.stringify(userData)
+            })
+
+            if(!response.ok) {
+                setError("Något gick fel vid registeringen. Försök igen senare"); 
+            } 
+
+            console.log("Användare skapad"); 
+
+        } catch(error) { 
+            console.log(error)
+        }
+
+    }
+
   return (
         <>
-        <form id="userForm">
+        <form onSubmit={registerUser}>
             <label htmlFor="userName">Användarnamn:</label> <br /><br />
-            <input type="text" id="userName" name="userName" /> <br /><br />
+            <input type="text" id="userName" name="userName" value={userData.user_name} 
+            onChange={(event) => setUserData({...userData, user_name: event.target.value})}
+            /> <br /><br />
 
             <label htmlFor="email">E-post:</label><br /><br />
-            <input type="text" id="email" name="email" /><br /><br />
+            <input type="text" id="email" name="email" value={userData.email}
+            onChange={(event) => setUserData({...userData, email: event.target.value})}
+            /><br /><br />
 
             <label htmlFor="password">Lösenord:</label><br /><br />
-            <input type="text" id="password" name="password" /> <br /><br />
+            <input type="text" id="password" name="password" value={userData.password}
+            onChange={(event) => setUserData({...userData, password: event.target.value})}
+            /> <br /><br />
 
             <input id="registerBtn" type="submit" value="Skapa användare"/><br />
         </form>
+        <span>{error && error}</span>
         <br />
         <InfoDiv />
         </>
