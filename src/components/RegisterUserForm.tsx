3c31e143d07859e15fa79b15a26ab98b154d1c75
Stina Persson <stpe1901@student.miun.se>
2025-03-10 10:04:02 +0100

    lagt in felmeddelanden i registreringsformuläret

--- src/components/RegisterUserForm.tsx
+++ src/components/RegisterUserForm.tsx
@@ -1,7 +1,6 @@
 import InfoDiv from "./InfoDiv"
 import "../css/UserForm.css"; 
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
 
 const RegisterUserForm = () => {
     //interface för ny användare
@@ -13,13 +12,33 @@ const RegisterUserForm = () => {
 
     //state för användare och för errormeddelanden 
     const [userData, setUserData] = useState<NewUser>({user_name:"", email:"", password:""}); 
-    const [error, setError] = useState<string>(); 
+    const [error, setError] = useState<string []>([]); 
 
     const registerUser = async (event : any) => {
 
         event.preventDefault(); 
 
-        try {
+        const errors = []; 
+
+        if(!userData.user_name) {
+            errors.push("Du måste ange ett användarnamn"); 
+        }
+
+        if(!userData.email) {
+            errors.push("Du måste ange en korrekt mailadress"); 
+        }
+
+        if(!userData.password) {
+            errors.push("Du måste ange ett lösenord"); 
+        }
+
+        if(errors.length > 0 ) {
+            setError(errors); 
+            return; 
+        }
+
+        try { 
+
             const response = await fetch("http://localhost:3000/adduser", {
                 method: "POST", 
                 headers: {
@@ -29,13 +48,13 @@ const RegisterUserForm = () => {
             })
 
             if(!response.ok) {
-                setError("Något gick fel vid registeringen. Försök igen senare"); 
+                setError(["Något gick fel vid registeringen. Försök igen senare"]); 
             } 
 
             console.log("Användare skapad");
 
             //skriver ut att användaren är skapad
-            setError("Användare skapad! Du kan nu logga in"); 
+            setError(["Användare skapad! Du kan nu logga in"]); 
 
             //rensar formulär efter input
             setUserData({user_name:"", email: "", password:""}) 
@@ -66,7 +85,7 @@ const RegisterUserForm = () => {
 
             <input id="registerBtn" type="submit" value="Skapa användare"/><br />
         </form>
-        <p>{error && error}</p>
+        {error.length > 0 && error.map((err, index) => <p key={index}>{err}</p>)}
         <br />
         <InfoDiv />
         </>
