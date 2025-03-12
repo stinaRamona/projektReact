05e8fb215046008c999aff7c348fedaa99ddc852
Stina Persson <stpe1901@student.miun.se>
2025-03-12 15:41:25 +0100

    Man kan skriva omdömen och de visas på boken som man är på

--- src/pages/SinglePage.tsx
+++ src/pages/SinglePage.tsx
@@ -15,17 +15,24 @@ const SinglePage = () => {
     } 
 
     interface ReviewForm {
+        _id: string,
         bookId: string, //ska vara från googlebooks 
         userId: string, //ska vara från användaren (redan fixat genom api)
         rating: number, 
         review: string
-      }; 
+    }; 
+
+    interface Review {
+        rating: number, 
+        review: string
+    }
 
     //states 
     const {id} = useParams<{id: string}>(); 
-    const [bookInfo, setBookInfo] = useState<Book>(); 
+    const [bookInfo, setBookInfo] = useState<Book>({id:"", image: "", title: "", author: "", description: ""}); 
     const [error, setError] = useState<string>(); 
-    const [reviewData, setReviewData] = useState<ReviewForm>({bookId : "", userId: "", rating: 1, review: ""})
+    const [reviewData, setReviewData] = useState<ReviewForm>({_id:"", bookId : "", userId: "", rating: 1, review: ""})
+    const [bookReview, setBookReview] = useState<Review[]>(); 
 
     const {user} = useAuth();
     
@@ -54,6 +61,22 @@ const SinglePage = () => {
 
         } catch(error) {
 
+            console.log(error); 
+        }
+
+    }
+
+    //Hämtar omdömen om boken
+    const getBookReviews = async () => {
+
+        try {
+            const response = await fetch("http://localhost:3000/review/" + id); 
+
+            if(response.ok) {
+                const data = await response.json(); 
+                setBookReview(data); 
+            }
+        } catch(error) {
             console.log(error); 
         }
 
@@ -92,12 +115,15 @@ const SinglePage = () => {
             console.log("Det gick inte att lägga till" + error); 
         }
 
-        setReviewData({bookId: "", userId:"", rating: 1, review: ""})
+        setReviewData({_id: "", bookId: "", userId:"", rating: 1, review: ""})
+
+        getBookReviews(); 
     }
 
     useEffect(() => {
         getBook();
-    }, [id]);
+        getBookReviews(); 
+    }, [id]); 
 
     return (
     <>
@@ -110,12 +136,24 @@ const SinglePage = () => {
     </div>
     <h3>Omdömen:</h3>
     <div id="bookReviewsDiv">
-        <p>Här ska omdömmena på boken finnas</p>
+        {!bookReview ? <p>Det finns inga recensioner på denna bok</p> :
+        <>
+        {bookReview.map((review, index) => (
+            <div className="reviewDiv" key={index}>
+                <h3>{review.rating}/5</h3>
+                <p>{review.review}</p>
+                <em>Recenserad av användare {user?.user_name}</em>
+            </div>
+        ))}
+        </> 
+        }
     </div>
     {/*Om man inte är inloggad så blir man hänvisad till att logga in om man vill recenserra boken. Annars visas ett formulär för det*/}
     {!user ? <p><NavLink className="inTextLink" to="/login">Logga in</NavLink> för att skriva en recension på boken. Har du inget konto? <NavLink className="inTextLink" to="/register">Registrera dig!</NavLink></p> : 
-    <form id="reviewForm">
+    
+    <form id="reviewForm" onSubmit={() => submitReview(bookInfo?.id, event)}>
         <h3>Lämna en recension på boken, {user.user_name}</h3>
+
         <label htmlFor="rating">Omdömme (1-5):</label><br /><br />
         <input type="number" id="rating" name="rating" value={reviewData.rating}
         onChange={(event) => setReviewData({...reviewData, rating: Number(event.target.value)})}
@@ -126,7 +164,7 @@ const SinglePage = () => {
         onChange={(event) => setReviewData({...reviewData, review: event.target.value})}
         ></textarea><br /><br />
 
-        <input type="submit" value={"Lämna omdömme"} id="newRevBtn" onClick={() => submitReview(bookInfo?.id, event)}/>
+        <input type="submit" value={"Lämna omdömme"} id="newRevBtn"/>
     </form>
     
     }
